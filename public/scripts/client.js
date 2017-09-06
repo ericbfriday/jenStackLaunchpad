cb1f19a71acb8cacb06212a81c36932cfc6485ab
Eric Friday <ericfriday@gmail.com>
2017-09-06 14:53:42 -0500

    finished base mode of partner activity - retrying to code it again

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,22 @@
-console.log( 'js' );
+ console.log( 'js' );
+
+$(document).ready( onReady );
+
+function onReady() {
+    console.log('onReady is loaded');
+    $( '#requestButton' ).on( 'click' , clickFunction);
+    $.ajax({
+        method: 'GET',
+        url: '/kittens',
+        success: function (response) {
+            console.log('back from server with ' + response);
+        }
+    });
+}
+
+function clickFunction() {
+    console.log('clickFunction called');
+
+}
+
+*/
--- server.js
+++ server.js
@@ -18,3 +18,7 @@ app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'views/index.html' ) );
 }); // end base url
 
+app.get( '/kittens' , function ( req, res) {
+  console.log('Inside app.get kittens');
+  res.send( 'I love kittens' );
+}); 
